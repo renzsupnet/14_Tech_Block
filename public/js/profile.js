85ad9de35a3822d3020ce6f37c8866ea427a0eed
Renz <renzlakers24@gmail.com>
2024-07-15 22:52:17 -0700

    Added handlebars, routes, and javascript logic.

---
+++ public/js/profile.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#blogPost-title').value.trim();
+    const description = document.querySelector('#blogPost-desc').value.trim();
+  
+    if (title && description) {
+      const response = await fetch(`/api/blogPosts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create Blog Post!');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogPosts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete Blog Post!');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blogPost-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.blogPost-list')
+    .addEventListener('click', delButtonHandler);
+  
